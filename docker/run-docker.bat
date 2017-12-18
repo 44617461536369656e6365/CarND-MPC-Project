1e3f39385b836726b99f9f1149dd18b1479c88e9
Paul Michalik <paul.michalik@outlook.com>
2017-12-18 13:31:46 +0100

    Add security options. Necessary to connect to gdbserver and to be able to listen to exposed port

--- docker/run-docker.bat
+++ docker/run-docker.bat
@@ -11,8 +11,8 @@ set "CurPath=%CurPath:\=/%"
 ::set "CurPath=%CurPath::=%"
 
 call "%~dp0docker-id.bat"
-docker run -d --rm -p 4567:4567 -p 22:22 --name %DockerContainerName% -v %CurPath%/..:/usr/src/%DockerProject% -w /usr/src/%DockerProject% %DockerUserName%/%DockerImageName%:%DockerImageTag%
+docker run -d --rm -p 4567:4567 -p 22:22 --security-opt seccomp:unconfined --name %DockerContainerName% -v %CurPath%/..:/usr/src/%DockerProject% -w /usr/src/%DockerProject% %DockerUserName%/%DockerImageName%:%DockerImageTag%
 
 rem Running "bash" command causes the entry point "user/sbin/sshd -D" not run or not run correctly
-
+rem Unfortunately, --security-opt seccomp:unconfined seems to be necessary for beeing able to connect to gdb server 
 endlocal
